28ede2eaa70209340af86fad52b901e6686d9c04
Jason Snider <root@jasonsnider.com>
2018-04-29 09:04:00 -0500

    Added init method

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,9 +3,17 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Flash');
+        $this->loadComponent('Paginator');
+    }
+
     public function index()
     {
-        $this->loadComponent('Paginator');
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
